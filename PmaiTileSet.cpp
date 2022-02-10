13169a4f96793f4e5f802624c8c1725a40b3f44c
Nuttadon <nuttadon_p@cmu.ac.th>
2022-02-10 19:52:10 +0700

    add stages/scoring sys

--- PmaiTileSet.cpp
+++ PmaiTileSet.cpp
@@ -5,6 +5,7 @@
 #include <dos.h>
 #include<fstream>
 #include <time.h> 
+
 #define STD_OUTPUT_HANDLE ((DWORD)-11)
 
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -12,6 +13,10 @@ COORD CursorPosition;
 HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
 using namespace std;
+class point{
+public:
+    int pts;
+};
 void p1();
 void p2();
 void gamestart();
@@ -19,6 +24,7 @@ void Howtoplay();
 void gameover();
 void menu();
 void role();
+void nextmap();
 void gotoxy( short x, short y ){
     COORD pos{x,y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
@@ -28,18 +34,46 @@ void space(int x, int y){
 	CursorPosition.Y = y;
 	SetConsoleCursorPosition(console, CursorPosition);
 }
-char mapset[5][20] = {
-"# _ _ # # # _ _ # #",  
+char mapset1[10][60] = {
+"O N _ # # # _ * # #",  
 "_ # _ _ _ 2 _ _ _ #",    
 "_ _ @ _ _ # _ _ # #",  
 "_ _ _ # _ # * _ # #",    
 "_ _ _ # _ _ # _ 1 #"};
-char map[5][20];
+char mapset2[10][60] = {
+"T W _ # # # _ * # #",  
+"_ @ _ _ @ 2 _ _ _ #",    
+"_ _ @ _ _ # _ _ # #",  
+"_ _ _ # _ # * _ # #",    
+"_ _ _ # _ _ # _ 1 #"};
+char mapset3[10][60] = {
+"T H _ # # # _ * # #",  
+"_ @ _ _ @ 2 _ _ _ #",    
+"_ _ @ _ _ # _ _ # #",  
+"_ _ _ # _ # * _ # #",    
+"_ _ _ # _ _ # _ 1 #"};
+char mapset4[10][60] = {
+"F O _ # # # _ * # #",  
+"_ @ _ _ @ 2 _ _ _ #",    
+"_ _ @ _ _ # _ _ # #",  
+"_ _ _ # _ # * _ # #",    
+"_ _ _ # _ _ # _ 1 #"};
+char mapset5[10][60] = {
+"F I _ # # # _ * # #",  
+"_ @ _ _ @ 2 _ _ _ #",    
+"_ _ @ _ _ # _ _ # #",  
+"_ _ _ # _ # * _ # #",    
+"_ _ _ # _ _ # _ 1 #"};
+char map[10][60];
 int x,y,i,j;
 int times=60;
 //spawnpoint
-int turn=0,P1=0,P2=0;
-bool game_runner = true;
+int turn=0,P1=0,P2=0;//determide turn and role 1=sur 2=finder
+bool game_runner = false;
+point play1;
+point play2;
+int maxpts;
+int maptemp=1;
 
 int main(){
 srand(time(0));
@@ -58,6 +92,7 @@ void p1(){
                 map[y][x] = '_';
                 y++; 
                 map[y][x] = '1';
+                play1.pts++;
             }if(map[yc][x] == '@'&&map[yc+1][x] == '_'){
                 map[y][x] = '_';
                 y++;
@@ -79,6 +114,7 @@ void p1(){
                 map[y][x] = '_'; 
                 y--;
                 map[y][x] = '1';
+                play1.pts++;
             }if(map[yc][x] == '@'&&map[yc-1][x] == '_'){
                 map[y][x] = '_';
                 y--;
@@ -101,6 +137,7 @@ void p1(){
                 map[y][x] = '_'; 
                 x+=2;
                 map[y][x] = '1';
+                play1.pts++;
             }if(map[y][xc] == '@'&&map[y][xc+2] == '_'){
                 map[y][x] = '_';
                 x+=2;
@@ -122,6 +159,7 @@ void p1(){
                 map[y][x] = '_'; 
                 x-=2;
                 map[y][x] = '1';
+                play1.pts++;
                 }if(map[y][xc] == '@'&&map[y][xc-2] == '_'){
                 map[y][x] = '_';
                 x-=2;
@@ -218,6 +256,7 @@ if(turn==2&&P2==1){
                 map[j][i] = '_'; 
                 j++;
                 map[j][i] = '2';
+                play2.pts++;
             }if(map[jc][i] == '@'&&map[jc+1][i] == '_'){
                 map[j][i] = '_';
                 j++;
@@ -240,6 +279,7 @@ if(turn==2&&P2==1){
                 map[j][i] = '_';
                 j--;
                 map[j][i] = '2'; 
+                play2.pts++;
             }if(map[jc][i] == '@'&&map[jc-1][i] == '_'){
                 map[j][i] = '_';
                 j--;
@@ -263,6 +303,7 @@ if(turn==2&&P2==1){
                 map[j][i] = '_'; 
                 i+=2;
                 map[j][i] = '2';
+                play2.pts++;
             }if(map[j][ic] == '@'&&map[j][ic+2] == '_'){
                 map[j][i] = '_';
                 i+=2;
@@ -284,6 +325,7 @@ if(turn==2&&P2==1){
                 map[j][i] = '_'; 
                 i-=2;
                 map[j][i] = '2';
+                play2.pts++;
             }if(map[j][ic] == '@'&&map[j][ic-2] == '_'){
                 map[j][i] = '_';
                 i-=2;
@@ -294,7 +336,7 @@ if(turn==2&&P2==1){
             Sleep(200);
             return;
         }
-
+    
     }
 if(turn==2&&P2==2){
         if(GetAsyncKeyState(0x53)){
@@ -366,7 +408,7 @@ if(turn==2&&P2==2){
             Sleep(200);
             return;
         }
-
+    
     }
     }
 void Howtoplay(){
@@ -470,13 +512,26 @@ void gameover(){
     }
 }
 void gamestart(){
-    for(int r=0;r<5;r++){
-        for(int c=0;c<20;c++){
-    map[r][c] = mapset[r][c];
-        }
+    game_runner = true;
+     play1.pts = 0;
+     play2.pts = 0;
+    if(maptemp==1) maxpts = 2;
+    if(maptemp==2) maxpts = 3;
+    if(maptemp==3) maxpts = 3;
+    if(maptemp==4) maxpts = 4;
+    if(maptemp==5) maxpts = 5;
+        
+    for(int r=0;r<10;r++){
+        for(int c=0;c<60;c++){
+    if(maptemp==1) map[r][c] = mapset1[r][c];
+    if(maptemp==2) map[r][c] = mapset2[r][c];
+    if(maptemp==3) map[r][c] = mapset3[r][c];
+    if(maptemp==4) map[r][c] = mapset4[r][c];
+    if(maptemp==5) map[r][c] = mapset5[r][c];
     }
-    for(int r=0;r<5;r++){
-        for(int c=0;c<20;c++){
+    }
+    for(int r=0;r<10;r++){
+        for(int c=0;c<60;c++){
             if(map[r][c]=='1') {
                 y=r;
                 x=c;
@@ -489,7 +544,7 @@ void gamestart(){
      Sleep(100);
     while(game_runner == true){
     system("cls");
-        for(int dp=0;dp<5;dp++){
+        for(int dp=0;dp<10;dp++){
             space(50,dp+5);
             cout << map[dp] << endl;
         }
@@ -499,18 +554,37 @@ void gamestart(){
                 cout << " |  Use WASD to move P1  | \n"; }
     else {gotoxy(45,11);  
     cout << " |  Use WASD to move P2  | \n"; }
+    gotoxy(45,12);
+    if(turn==1&&P1==1) {
+        cout << " |    Your score is "<<play1.pts<<"/"<<maxpts<<"  | \n";
+        gotoxy(45,13); 
+        cout<<" -------------------------- \n"; }
+    else if(turn==2&&P2==1) {
+        cout << " |    Your score is "<<play2.pts<<"/"<<maxpts<<"  | \n";
+        gotoxy(45,13); 
+        cout<<" -------------------------- \n";}
+    else {
     gotoxy(45,12); 
     cout<<" -------------------------- \n";
+    }
+    if(P1==1&&play1.pts==maxpts){
+        maptemp++;
+        gamestart();
+    }
+    if(P2==1&&play2.pts==maxpts){
+        maptemp++;
+        gamestart();
+    }
     system("pause>nul");
             p1();
             p2();
     }
-}
+    }
+
+
 void menu(){
 do{
 int randomcolor;
-srand(time(0));
-int a[randomcolor] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 int len = 0,x;
 string text;
 len = text.length();
@@ -519,7 +593,7 @@ SetConsoleTextAttribute(h,6);
 space(50,5); text = " -------------------------- \n";
 len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
@@ -527,14 +601,14 @@ len = text.length();
 space(50,6); text = " |      LostHope Game     | \n";
 len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
 space(50,7); text = " --------------------------\n";
 len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
@@ -542,28 +616,28 @@ SetConsoleTextAttribute(h,14);
 space(50,9);  text = "  1. Start Game \n";
 len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
 space(50,10); text = "  2. How to play  \n";	 
 len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
 space(50,11); text = "  3. Quit\n";
 len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
 space(50,13); text = "Select option: ";
 len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
@@ -579,7 +653,6 @@ void role(){
     while(P1==0){
     int randomcolor;
 	srand(time(0));
-	int a[randomcolor] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 	int len = 0,x;
     string text;
 	len = text.length();
@@ -588,49 +661,49 @@ void role(){
 	space(50,5); text = " --------------------------- \n";
     len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
 	space(50,6); text = " |     choose role P1      | \n";
     len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
 	space(50,7);text = " ---------------------------\n";
     len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
     space(50,9);  text = "1. Survival \n";
     len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
     space(50,10); text = "2. Finder \n";
     len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
     space(50,11); text = "3. Quit \n";
     len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
     space(50,13); text = "Select option: ";
     len = text.length();
 	for(x=0;x<len;x++){
-	    randomcolor = rand()%15;
+	    randomcolor = rand()%15+1;
 		SetConsoleTextAttribute(h,randomcolor);
 		cout << text[x];	
     }
@@ -649,6 +722,8 @@ void role(){
 
 	if( P1 ==1) turn =1;
 	else if( P2 == 1 ) turn=2;
-	space(50,9); cout<<"1. Next \n";
-	space(50,10);cout<<"2. Quit \n";
-	space(50,13);cout<<"Select option: ";
+    space(52,8);
+	cout << "Press any key to START";
+	char option = getche();
+    gamestart();
+}
